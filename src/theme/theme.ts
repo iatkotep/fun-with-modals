6de76adff20d12b0b2e00c61ba795101995d53af
David Lawson <davidlawson@Davids-MacBook-Pro-2.local>
2023-06-05 14:48:55 -0600

    Cleanup unused.  Define new nonstandard breakpt

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,17 +1,4 @@
 import { extendTheme } from "@chakra-ui/react";
-import { components, colors, typography, styles } from "./_";
-import { TGenericObject } from "../global/types";
-
-export type TUIStyles = {
-  [key: string]: any;
-  base?: TGenericObject;
-  sm?: TGenericObject;
-  smm?: TGenericObject;
-  md?: TGenericObject;
-  lg?: TGenericObject;
-  xl?: TGenericObject;
-  xxl?: TGenericObject;
-};
 
 export type TBreakpoints = {
   [key: string]: number;
@@ -20,20 +7,22 @@ export type TBreakpoints = {
 interface ISystemBreakpoints extends TBreakpoints {
   base: number;
   sm: number;
-  smm: number;
   md: number;
   lg: number;
   xl: number;
   xxl: number;
 }
 export const SystemBreakpoints: ISystemBreakpoints = {
+  // Standard
   base: 0,
   sm: 320,
-  smm: 500,
-  md: 600,
+  md: 768,
   lg: 960,
   xl: 1200,
   xxl: 1536,
+
+  // Non-standard
+  modalDesktop: 600,
 };
 
 const breakpoints = Object.entries(SystemBreakpoints).reduce(
