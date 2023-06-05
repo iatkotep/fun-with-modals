00f9819837682071165ac470d6982e73a2302614
David Lawson <davidlawson@Davids-MacBook-Pro-2.local>
2023-06-05 14:50:07 -0600

    Better more predictable structure for story compnt

--- src/components/useModal/test/ModalStory.tsx
+++ src/components/useModal/test/ModalStory.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import useModal, { IModal } from "../useModal";
 import { Box, Button, Flex } from "@chakra-ui/react";
 import { isNull } from "../../../global/helpers";
+import { StoryHeading } from "./StoryHeading";
+import { StoryBody } from "./StoryBody";
 
-interface IModalStory extends IModal {
+interface IModalStory {
+  storyTitle: string;
+  storyDescription: React.ReactNode;
+  modalProps: IModal;
   isOpenInit?: boolean;
+  storyButtonText?: string;
 }
 
 const flexCellProps = {
@@ -13,47 +19,29 @@ const flexCellProps = {
 };
 
 const ModalStory: React.FC<IModalStory> = ({
-  title,
-  body,
-  actions,
+  storyTitle,
+  storyDescription,
+  modalProps,
   isOpenInit = false,
+  storyButtonText = "Launch Modal",
 }) => {
-  const {
-    Modal: DismissibleModal,
-    openModal: openDismissableModal,
-    selectedActionId: selectedActionId_dismissible,
-  } = useModal(isOpenInit);
+  const { Modal, openModal, selectedActionId } = useModal(isOpenInit);
 
-  const {
-    Modal: BlockingModal,
-    openModal: openBlockingModal,
-    selectedActionId: selectedActionId_blocking,
-  } = useModal(isOpenInit);
   return (
-    <Flex w={"100%"}>
-      <Box {...flexCellProps}>
-        <Box mb={5}>
-          <Button onClick={() => openDismissableModal()}>
-            Launch Dismissible Version
-          </Button>
-          <DismissibleModal {...{ title, body, actions }} />
+    <>
+      <StoryHeading {...{ storyTitle, storyDescription }} />
+      <Flex w={"100%"}>
+        <Box {...flexCellProps}>
+          <Box mb={5}>
+            <Button onClick={() => openModal()}>{storyButtonText}</Button>
+            <Modal {...modalProps} />
+          </Box>
+          {!isNull(selectedActionId) && (
+            <Box>You selected "{modalProps.actions[selectedActionId!]}"</Box>
+          )}
         </Box>
-        {!isNull(selectedActionId_dismissible) && (
-          <Box>You selected "{actions[selectedActionId_dismissible!]}"</Box>
-        )}
-      </Box>
-      <Box {...flexCellProps}>
-        <Box mb={5}>
-          <Button onClick={() => openBlockingModal()}>
-            Launch Blocking Version
-          </Button>
-          <BlockingModal {...{ title, body, actions, isBlocking: true }} />
-        </Box>
-        {!isNull(selectedActionId_blocking) && (
-          <Box>You selected "{actions[selectedActionId_blocking!]}"</Box>
-        )}
-      </Box>
-    </Flex>
+      </Flex>
+    </>
   );
 };
 
