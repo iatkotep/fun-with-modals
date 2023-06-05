570cea962c6d2048f8bcce99d933ffeedbb00a30
David Lawson <davidlawson@Davids-MacBook-Pro-2.local>
2023-06-05 14:49:19 -0600

    cleanup.  use new Modal subcomponents

--- src/components/useModal/useModal.tsx
+++ src/components/useModal/useModal.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import ReactDOM from "react-dom";
-import { Box, Button, Link, Text } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 import { EjectionClassName, modalRoot } from "./helpers";
 import {
   wrapProps,
@@ -14,12 +14,17 @@ import {
 } from "./props";
 import { useEngagementLogic } from "./hooks/useEngagementLogic";
 import { TActionId, TOpenModal, useModalState } from "./hooks/useModalState";
+import ModalIcon from "./ModalIcon";
+import ModalBody from "./ModalBody/ModalBody";
+
+export type TModalIconType = "info" | "warning" | "error";
 
 export interface IModal {
   title: string;
   body: React.ReactNode;
   actions: string[];
   isBlocking?: boolean;
+  iconType?: TModalIconType;
 }
 
 type TUseModal = (isOpenInit?: boolean) => {
@@ -38,8 +43,9 @@ export const useModal: TUseModal = (isOpenInit = false) => {
   } = useModalState(isOpenInit);
 
   return {
-    Modal: ({ title, body, isBlocking = false, actions }) => {
+    Modal: ({ title, body, isBlocking = false, actions, iconType }) => {
       const refModalWrap = useRef<HTMLElement>(null);
+
       useEngagementLogic(refModalWrap, closeModal, selectActionId, isBlocking);
       if (!isModalOpen) return null;
 
@@ -57,10 +63,13 @@ export const useModal: TUseModal = (isOpenInit = false) => {
               />
             )}
             <Box {...createDialogSectionProps("static")}>
-              <Text {...titleProps}>{title}</Text>
+              <Text {...titleProps}>
+                {iconType && <ModalIcon type={iconType} />}
+                {title}
+              </Text>
             </Box>
             <Box {...createDialogSectionProps("dynamic")}>
-              <Text>{body}</Text>
+              <ModalBody>{body}</ModalBody>
             </Box>
             <Box {...createDialogSectionProps("static")}>
               <Box {...actionsProps}>
